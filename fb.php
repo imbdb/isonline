4e0c977a7ef118ab7262364084a7793bd07a59ce
root <root@localhost.localdomain>
2014-07-20 16:24:55 +0530

    ...

--- fb.php
+++ fb.php
@@ -7,16 +7,16 @@ require_once 'lib/Unirest.php';
 
   $facebook = new Facebook(array(
        'cookie' => true,
- 'appId' => '1407498822824763',
-    'secret' => '8f7d8b6206aa71e7a89bf82ea2889a7f',
+ 'appId' => '',
+    'secret' => 'f',
    ));   
 
-$facebook->setAccessToken('CAAUAHNtbMzsBAG36w8WUSeGYJOUhhJitGkkkz6dsEmKvOUAP8DYFMBaOjii7ucr6694kIZAArdfTyLKh6DrEFjVhVFVnSl7k8JpvB8XSWbsbunvLlDl4ZBAZBxnwmexA1Pyjq7joZAD4qUvK4FAOlu2yNoQjAwrZAEXGoZBnYquBjVzm1ZAJkyE');
- $fql = "select online_presence from user where uid in (select uid2 FROM friend WHERE uid1 = 100001545172176)  and uid=100007111897746";
+$facebook->setAccessToken('');
+ $fql = "select online_presence from user where uid in (select uid2 FROM friend WHERE uid1 = '')  and uid=''";
 
   $param  =   array(
       'method'    => 'fql.query',
-      'access_token' => 'CAAUAHNtbMzsBAG36w8WUSeGYJOUhhJitGkkkz6dsEmKvOUAP8DYFMBaOjii7ucr6694kIZAArdfTyLKh6DrEFjVhVFVnSl7k8JpvB8XSWbsbunvLlDl4ZBAZBxnwmexA1Pyjq7joZAD4qUvK4FAOlu2yNoQjAwrZAEXGoZBnYquBjVzm1ZAJkyE',
+      'access_token' => '',
       'query'     => $fql,
       'callback'  => ''
 );
@@ -33,39 +33,7 @@ echo($sta);
 
 if ( $fd <> $sta)
 {
-	file_put_contents($file,$sta);
-	if($sta == 'active')
-	{
-	$res = Unirest::get(
- 	"https://160by2.p.mashape.com/index.php?msg=On&phone=9429920346&pwd=9852&uid=9429920346",
-  	array(
-    	"X-Mashape-Authorization" => "x86f4rGZ8Xm8Mv0lZzlyPNnybhMACJmj"
-  	),
-  	null
-	);
-
-	}
-        if($sta == 'idle')
-	{
-	$res = Unirest::get(
- 	"https://160by2.p.mashape.com/index.php?msg=idl&phone=9429920346&pwd=9852&uid=9429920346",
-  	array(
-    	"X-Mashape-Authorization" => "x86f4rGZ8Xm8Mv0lZzlyPNnybhMACJmj"
-  	),
-  	null
-	);
-
-	}
-	if($sta == 'offline')
-	{
-	$res = Unirest::get(
- 	"https://160by2.p.mashape.com/index.php?msg=Off&phone=9429920346&pwd=9852&uid=9429920346",
-  	array(
-    	"X-Mashape-Authorization" => "x86f4rGZ8Xm8Mv0lZzlyPNnybhMACJmj"
-  	),
-        null
-        );
-	}
+	echo $sta;
 }
 */
 ?>
